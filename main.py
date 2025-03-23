625dd273793c0098d3c04b7f59b725bd92c01acd
Gaz <karimovgaz72@gmail.com>
2025-03-23 21:50:35 +0500

    Удалил вторую половину лишних проверок

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 try:
     number = int(input("""Выберите число от "1" до "5" """))
-    if 1 <= number <= 5:
-        if number == 1:
-            print("Соответствующее число - one")
-        elif number == 2:
-            print("Соответствующее число - two")
-        elif number == 3:
-            print("Соответствующее число - three")
-        elif number == 4:
-            print("Соответствующее число - four")
-        elif number == 5:
-            print("Соответствующее число - five")
-        else:
-            print("Введите пожалуйста коректное число.")
+    if number == 1:
+        print("Соответствующее число - one")
+    elif number == 2:
+        print("Соответствующее число - two")
+    elif number == 3:
+        print("Соответствующее число - three")
+    elif number == 4:
+        print("Соответствующее число - four")
+    elif number == 5:
+        print("Соответствующее число - five")
+    else:
+        print("Введите пожалуйста коректное число.")
 
 except ValueError:
     print("Ошибка: введено не число!")
