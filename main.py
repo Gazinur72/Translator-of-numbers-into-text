ef80a470b64ce26edadaa5167b842ba41f6e2f7b
Gaz <karimovgaz72@gmail.com>
2025-03-23 16:21:31 +0500

    Написал программу по выбору числа от 1 до 5 , которая показывает как пишется слово на английски для соответствующей цифры.

---
+++ main.py
@@ -0,0 +1,13 @@
+number = int(input("""Выберите число от "1" до "5" """))
+if number == 1:
+    print("Соответствующее число - one")
+elif number == 2:
+    print("Соответствующее число - two")
+elif number == 3:
+    print("Соответствующее число - three")
+elif number == 4:
+    print("Соответствующее число - four")
+elif number == 5:
+    print("Соответствующее число - five")
+else:
+    print("Введите пожалуйста коректное число.")
