26fbd0960448f25a8c779c4196e97affc10f78ac
Gaz <karimovgaz72@gmail.com>
2025-03-23 20:15:04 +0500

    Добавлен обработчик ошибок ввод символов заместо чисел, добалена проверка на ввод чисел из допустимого диапазона.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-number = int(input("""Выберите число от "1" до "5" """))
-if number == 1:
-    print("Соответствующее число - one")
-elif number == 2:
-    print("Соответствующее число - two")
-elif number == 3:
-    print("Соответствующее число - three")
-elif number == 4:
-    print("Соответствующее число - four")
-elif number == 5:
-    print("Соответствующее число - five")
-else:
-    print("Введите пожалуйста коректное число.")
+try:
+    number = int(input("""Выберите число от "1" до "5" """))
+    if 1 <= number <= 5:
+        if number == 1:
+            print("Соответствующее число - one")
+        elif number == 2:
+            print("Соответствующее число - two")
+        elif number == 3:
+            print("Соответствующее число - three")
+        elif number == 4:
+            print("Соответствующее число - four")
+        elif number == 5:
+            print("Соответствующее число - five")
+        else:
+            print("Введите пожалуйста коректное число.")
+    else:print("Введено число не входящее в доступный диапозон чисел. ")
+except ValueError:
+    print("Ошибка: введено не число!")
